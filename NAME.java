bda2c5a7b0a3c82f2954c4b0f68c68d55931b675
Edem Essang <edemoski.eu@gmail.com>
2021-08-20 00:09:13 +0100

    a simple program to print name in java

---
+++ NAME.java
@@ -0,0 +1,8 @@
+// a simple program to print my name in java
+public class NAME{
+    public static void main(String[] args) {
+        String myName = "Edem Essang Ukoh";
+        System.out.println("My name is:" + myName);
+        
+    }
+}
